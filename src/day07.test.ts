e39d0769ba39c1a3a4e74b9e7605fcc2db121c9b
Chiang Fong Lee <myself@cflee.net>
2023-12-12 22:23:51 +0800

    wip: day07

--- src/day07.test.ts
+++ src/day07.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@jest/globals";
-import { process1, process2 } from "./day06";
+import { compareTypedHands, getTypedHand2, process1, process2 } from "./day07";
 import { getNonEmptyLines, readFileToString } from "./utils";
-import { parseInput, typeHand } from "./day07";
+import { parseInput, getTypedHand } from "./day07";
 
 const sampleInput1 = `32T3K 765
 T55J5 684
@@ -24,77 +24,193 @@ test("parseInput", () => {
   ]);
 });
 
-test("typeHand", () => {
-  expect(typeHand({ cards: "AAAAA", bid: 1 })).toEqual({
+test("getTypedHand", () => {
+  expect(getTypedHand({ cards: "AAAAA", bid: 1 })).toEqual({
     type: 7,
     cards: "AAAAA",
     bid: 1,
   });
-  expect(typeHand({ cards: "AA8AA", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "AA8AA", bid: 1 })).toEqual({
     type: 6,
     cards: "AA8AA",
     bid: 1,
   });
-  expect(typeHand({ cards: "23332", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "23332", bid: 1 })).toEqual({
     type: 5,
     cards: "23332",
     bid: 1,
   });
-  expect(typeHand({ cards: "TTT98", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "TTT98", bid: 1 })).toEqual({
     type: 4,
     cards: "TTT98",
     bid: 1,
   });
-  expect(typeHand({ cards: "23432", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "23432", bid: 1 })).toEqual({
     type: 3,
     cards: "23432",
     bid: 1,
   });
-  expect(typeHand({ cards: "A23A4", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "A23A4", bid: 1 })).toEqual({
     type: 2,
     cards: "A23A4",
     bid: 1,
   });
-  expect(typeHand({ cards: "23456", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "23456", bid: 1 })).toEqual({
     type: 1,
     cards: "23456",
     bid: 1,
   });
 
-  expect(typeHand({ cards: "8AAAA", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "8AAAA", bid: 1 })).toEqual({
     type: 6,
     cards: "8AAAA",
     bid: 1,
   });
-  expect(typeHand({ cards: "89TTT", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "89TTT", bid: 1 })).toEqual({
     type: 4,
     cards: "89TTT",
     bid: 1,
   });
-  expect(typeHand({ cards: "43322", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "43322", bid: 1 })).toEqual({
     type: 3,
     cards: "43322",
     bid: 1,
   });
-  expect(typeHand({ cards: "234AA", bid: 1 })).toEqual({
+  expect(getTypedHand({ cards: "234AA", bid: 1 })).toEqual({
     type: 2,
     cards: "234AA",
     bid: 1,
   });
 });
 
+test("compareTypedHands", () => {
+  expect(
+    compareTypedHands(
+      getTypedHand({ cards: "33332", bid: 1 }),
+      getTypedHand({ cards: "2AAAA", bid: 1 }),
+      1,
+    ),
+  ).toBeGreaterThan(0);
+  expect(
+    compareTypedHands(
+      getTypedHand({ cards: "77888", bid: 1 }),
+      getTypedHand({ cards: "77788", bid: 1 }),
+      1,
+    ),
+  ).toBeGreaterThan(0);
+
+  const typedHands = parseInput(getNonEmptyLines(sampleInput1)).map((hand) =>
+    getTypedHand(hand),
+  );
+  typedHands.sort((a, b) => compareTypedHands(a, b, 1));
+  expect(typedHands[0].cards).toEqual("32T3K");
+  expect(typedHands[1].cards).toEqual("KTJJT");
+  expect(typedHands[2].cards).toEqual("KK677");
+  expect(typedHands[3].cards).toEqual("T55J5");
+  expect(typedHands[4].cards).toEqual("QQQJA");
+});
+
 test("part 1 example", () => {
   expect(process1(sampleInput1)).toBe(6440);
 });
 
-// test("part 1 regression", () => {
-//   expect(process1(input)).toBe(32076);
-// });
+test("part 1 regression", () => {
+  expect(process1(input)).toBe(250254244);
+});
 
-// test("part 2 example", () => {
-//   expect(process2(sampleInput1)).toBe(71503);
-// });
+test("getTypedHand2", () => {
+  expect(getTypedHand2({ cards: "QJJQ2", bid: 1 })).toEqual({
+    type: 6,
+    cards: "QJJQ2",
+    bid: 1,
+  });
 
-// test("part 2 regression", () => {
-//   expect(process2(input)).toBe(34278221);
-// });
+  expect(getTypedHand2({ cards: "32T3K", bid: 1 })).toEqual({
+    type: 2,
+    cards: "32T3K",
+    bid: 1,
+  });
+  expect(getTypedHand2({ cards: "T55J5", bid: 1 })).toEqual({
+    type: 6,
+    cards: "T55J5",
+    bid: 1,
+  });
+  expect(getTypedHand2({ cards: "KK677", bid: 1 })).toEqual({
+    type: 3,
+    cards: "KK677",
+    bid: 1,
+  });
+  expect(getTypedHand2({ cards: "KTJJT", bid: 1 })).toEqual({
+    type: 6,
+    cards: "KTJJT",
+    bid: 1,
+  });
+  expect(getTypedHand2({ cards: "QQQJA", bid: 1 })).toEqual({
+    type: 6,
+    cards: "QQQJA",
+    bid: 1,
+  });
+
+  expect(getTypedHand2({ cards: "J33JJ", bid: 1 })).toEqual({
+    type: 7,
+    cards: "J33JJ",
+    bid: 1,
+  });
+});
+
+test("compareTypedHands2", () => {
+  expect(
+    compareTypedHands(
+      getTypedHand2({ cards: "JKKK2", bid: 1 }),
+      getTypedHand2({ cards: "QQQQ2", bid: 1 }),
+      2,
+    ),
+  ).toBeLessThan(0);
+  expect(
+    compareTypedHands(
+      getTypedHand2({ cards: "QQQQ2", bid: 1 }),
+      getTypedHand2({ cards: "JKKK2", bid: 1 }),
+      2,
+    ),
+  ).toBeGreaterThan(0);
+  expect(
+    compareTypedHands(
+      getTypedHand2({ cards: "JJJJ1", bid: 1 }),
+      getTypedHand2({ cards: "2JJJJ", bid: 1 }),
+      2,
+    ),
+  ).toBeLessThan(0);
+  expect(
+    compareTypedHands(
+      getTypedHand2({ cards: "JJ33K", bid: 1 }),
+      getTypedHand2({ cards: "2222Q", bid: 1 }),
+      2,
+    ),
+  ).toBeLessThan(0);
+  expect(
+    compareTypedHands(
+      getTypedHand2({ cards: "98764", bid: 1 }),
+      getTypedHand2({ cards: "98763", bid: 1 }),
+      2,
+    ),
+  ).toBeGreaterThan(0);
+
+  const typedHands = parseInput(getNonEmptyLines(sampleInput1)).map((hand) =>
+    getTypedHand2(hand),
+  );
+  typedHands.sort((a, b) => compareTypedHands(a, b, 2));
+  expect(typedHands[0].cards).toEqual("32T3K");
+  expect(typedHands[1].cards).toEqual("KK677");
+  expect(typedHands[2].cards).toEqual("T55J5");
+  expect(typedHands[3].cards).toEqual("QQQJA");
+  expect(typedHands[4].cards).toEqual("KTJJT");
+});
+
+test("part 2 example", () => {
+  expect(process2(sampleInput1)).toBe(5905);
+});
+
+test("part 2 regression", () => {
+  // 249609189 is too low
+  expect(process2(input) != 249609189).toEqual(true);
+});
--- src/day07.ts
+++ src/day07.ts
@@ -1,4 +1,4 @@
-import { getNonEmptyLines } from "./utils";
+import { getNonEmptyLines, sumNumbers } from "./utils";
 
 interface Hand {
   cards: string;
@@ -19,7 +19,7 @@ export const parseInput = (lines: string[]): Hand[] => {
   });
 };
 
-export const typeHand = (hand: Hand): TypedHand => {
+export const getTypedHand = (hand: Hand): TypedHand => {
   let bestType = 0;
   for (let i = 0; i < 5; i++) {
     const matches = hand.cards.match(new RegExp(hand.cards.charAt(i), "g"));
@@ -77,12 +77,106 @@ export const typeHand = (hand: Hand): TypedHand => {
   };
 };
 
-export const process1 = (input: string): number => {
-  const lines: string[] = getNonEmptyLines(input);
+export const getTypedHand2 = (hand: Hand): TypedHand => {
+  const cardCounts = new Map();
+  for (let i = 0; i < 5; i++) {
+    const card = hand.cards.charAt(i);
+    if (!cardCounts.has(card)) {
+      cardCounts.set(card, 1);
+    } else {
+      cardCounts.set(card, cardCounts.get(card) + 1);
+    }
+  }
+
+  let jokerCount = cardCounts.get("J") ?? 0;
+  cardCounts.delete("J");
+  const cardCountValues = [...cardCounts.values()];
+  cardCountValues.sort().reverse();
+  cardCountValues[0] += jokerCount;
+
+  let bestType = 0;
+  if (cardCountValues[0] == 5) {
+    bestType = 7;
+  } else if (cardCountValues[0] == 4) {
+    bestType = 6;
+  } else if (cardCountValues[0] == 3) {
+    if (cardCountValues[1] == 2) {
+      bestType = 5;
+    } else {
+      bestType = 4;
+    }
+  } else if (cardCountValues[0] == 2) {
+    if (cardCountValues[1] == 2) {
+      bestType = 3;
+    } else {
+      bestType = 2;
+    }
+  } else {
+    bestType = 1;
+  }
+  return {
+    type: bestType,
+    ...hand,
+  };
+};
+
+export const makeNumeric = (label: string, part: number): number => {
+  switch (label) {
+    case "A":
+      return 14;
+    case "K":
+      return 13;
+    case "Q":
+      return 12;
+    case "J":
+      if (part == 1) {
+        return 11;
+      } else {
+        return 1;
+      }
+    case "T":
+      return 10;
+    default:
+      return parseInt(label, 10);
+  }
+};
+
+export const compareCards = (a: string, b: string, part: number): number => {
+  const an = makeNumeric(a, part);
+  const bn = makeNumeric(b, part);
+  return an - bn;
+};
+
+export const compareTypedHands = (
+  a: TypedHand,
+  b: TypedHand,
+  part: number,
+): number => {
+  if (a.type != b.type) {
+    // weaker types (lower value) come first
+    return a.type - b.type;
+  }
+  for (let i = 0; i < 5; i++) {
+    const c = compareCards(a.cards.charAt(i), b.cards.charAt(i), part);
+    if (c != 0) {
+      return c;
+    }
+  }
   return 0;
 };
 
+export const process1 = (input: string): number => {
+  const typedHands = parseInput(getNonEmptyLines(input)).map((hand) =>
+    getTypedHand(hand),
+  );
+  typedHands.sort((a, b) => compareTypedHands(a, b, 1));
+  return typedHands.map((hand, idx) => hand.bid * (idx + 1)).reduce(sumNumbers);
+};
+
 export const process2 = (input: string): number => {
-  const lines: string[] = getNonEmptyLines(input);
-  return 0;
+  const typedHands = parseInput(getNonEmptyLines(input)).map((hand) =>
+    getTypedHand2(hand),
+  );
+  typedHands.sort((a, b) => compareTypedHands(a, b, 2));
+  return typedHands.map((hand, idx) => hand.bid * (idx + 1)).reduce(sumNumbers);
 };
